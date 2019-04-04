04a2eb26bb9fc439c3651c2694b1ea75492b2a4e
Mell <30361727+mellandrade@users.noreply.github.com>
2019-04-04 08:08:32 -0700

    Add files via upload
    A5 - Update01

---
+++ Assignment05/js/dom-practice.js
@@ -0,0 +1,26 @@
+/*eslint-env browser*/
+
+//STEP 1
+
+document.getElementById("myBtn").addEventListener("click", function () {
+    "use strict";
+    window.alert("I have been clicked.");
+});
+
+//STEP 2
+
+//STEP 3
+
+//STEP 4
+
+//STEP 5
+
+//STEP 6
+
+//STEP 7
+
+//STEP 8
+
+//STEP 9
+
+//STEP 10
